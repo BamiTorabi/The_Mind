852ac1b0c3bf9e01b958367b4be59e930804c2cd
BamiTorabi <bamdadtorabi99@gmail.com>
2022-06-29 23:49:00 +0430

    Last changes for 29/06/2022

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,25 +5,32 @@ import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
 
+import static server.ServerStatus.OPEN;
+
 public class Server {
 
     private static Server server = null;
     private List<ClientHandler> handlers;
+    private ServerStatus status;
     final private int port = 8080;
 
     private Server (){
         handlers = new ArrayList<>();
-        try{
-            ServerSocket server = new ServerSocket(port);
-        } catch (IOException e){
-            System.out.println(e.getMessage());
-        }
+        status = OPEN;
     }
 
     public static Server getInstance(){
         if (server == null)
             server = new Server();
         return server;
+    }
+
+    public void init(){
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public int getPort() {
--- src/main/java/server/ServerMain.java
+++ src/main/java/server/ServerMain.java
@@ -4,5 +4,6 @@ public class ServerMain {
 
     public static void main(String[] args) {
         Server server = Server.getInstance();
+        server.init();
     }
 }
