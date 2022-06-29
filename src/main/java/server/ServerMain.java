1b0ee90c9f4cdd8c95172b5774986fc2cb873575
BamiTorabi <bamdadtorabi99@gmail.com>
2022-06-29 17:22:54 +0430

    Server add

---
+++ src/main/java/server/ServerMain.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ServerMain {
+
+    static private int port = 9000;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(port);
+
+    }
+}
